ceb88f291e8351225c64414f7f1e4b93acf2048e
Jack Cheng <jqcheng9413@yahoo.com>
2024-02-24 11:22:09 +1300

    add plotly visualizations

--- application/routes.py
+++ application/routes.py
@@ -1,23 +1,36 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import io
+import plotly.express as px
+from plotly import utils
+import plotly 
+import json
 from application import app
 from flask import render_template
-
-# read the data
-df = pd.read_csv('kc_house_data.csv')
 
 
 @app.route('/')
 def index():
-    fig,ax=plt.subplots(figsize=(6,6))
-    ax=sns.set(style="darkgrid")
-    sns.heatmap(df[["price", "bedrooms", "bathrooms", "sqft_living", "sqft_above", "sqft_basement"]].corr(), annot=True, cmap='coolwarm', center=0)
-    canvas=FigureCanvas(fig)
-    img = io.BytesIO()
-    fig.savefig(img)
-    img.seek(0)
-    return render_template('index.html', send_file=img, mimetype='image/png')
+    # Load the data
+    df = pd.read_csv(r'kc_house_data.csv')
+    
+    # use plotly to create a scatter plot
+    fig = px.scatter(df, x='sqft_living', y='price', title='Price vs Square Feet', height=500, width=700)
+    
+    # convert the figure to a json object by using the utils.PlotlyJSONEncoder
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # use plotly to create a heatmap to show the correlation between the features
+    fig2 = px.imshow(df[["price", "bedrooms", "bathrooms", "sqft_living", "sqft_above", "sqft_basement"]].corr(), 
+                     title='Correlation Matrix', height=500, width=700)
+    
+    # convert the figure to a json object by using the utils.PlotlyJSONEncoder
+    graphJSON2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # use plotly to create a box plot to show the relationship between the number of bedrooms and the price
+    fig3 = px.box(df, x='bedrooms', y='price', title='Price vs Bedrooms', height=500, width=700)
+    
+    # convert the figure to a json object by using the utils.PlotlyJSONEncoder
+    graphJSON3 = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    return render_template('index.html', graphJSON=graphJSON, graphJSON2=graphJSON2, graphJSON3=graphJSON3)
 
