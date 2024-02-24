44c03af38ae2d6f522924fcf1b24069898016760
Jack Cheng <jqcheng9413@yahoo.com>
2024-02-24 17:36:11 +1300

    add data model

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = '123hdchfuhudhfnfggsojkdjhunjnckjkdfhbapqowomdncioi99090kmcnjdh?!&~'
+
+db = SQLAlchemy(app)
+
+app.app_context().push()
 
 from application import routes
